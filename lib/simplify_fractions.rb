c15bab16d0bf371b40da275dd7ad68b846f7d6fb
Fabian Bermel <fabian.bermel@gfk.com>
2016-08-12 14:15:37 +0200

    Add module SimplifyFractions Add class SimplifyFractions add class find_gcd add class dimplify_fraction

--- lib/simplify_fractions.rb
+++ lib/simplify_fractions.rb
@@ -1,5 +1,16 @@
 require "simplify_fractions/version"
 
 module SimplifyFractions
-  # Your code goes here...
+  class SimplifyFractions
+    def find_gcd(numerator,denominator)
+      gcd = numerator % denominator
+      unless gcd == 0
+        gcd = numerator % denominator
+      end
+    end
+
+    def simplify_fraction(numerator,denominator)
+      return numerator / self.find_gcd(numerator,denominator), denominator / self.find_gcd(numerator,denominator)
+    end
+  end
 end
